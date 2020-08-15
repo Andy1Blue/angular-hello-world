d997b7ba38f267377028f8746b5368e3c3de452e
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2020-08-15 11:21:52 +0200

    Change project name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularFirst
+# AngularHelloWorld
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.5.
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-first'),
+      dir: require('path').join(__dirname, './coverage/angular-hello-world'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
